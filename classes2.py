53431d4a36f1aa8f2ef0d957c64346ade7c1dfdb
jonalynn masters <jonalynna.masters@gmail.com>
2020-04-08 11:10:02 -0700

    classes round 2

--- classes2.py
+++ classes2.py
@@ -1,4 +1,50 @@
 '''
 A store can have multiple departments. A department can hold multiple products. 
 The store has a name. Departments have names. Products have names and prices.
+
+Nouns tend to be classes
+If a noun"has" something, that something tends to be an attribute.
+Verbs tend to be methods.
+
 '''
+
+
+class Store:
+    """ Holds info about a Store aka documentation string"""
+
+    def __init__(self, name, departments=None):
+        """ Construct a new Store"""
+        self.name = name
+        self.departments = []
+
+        if departments is None:
+            self.departments = []
+        else:
+            self.departments = departments
+
+
+class Departments:
+    def __init__(self, name):
+        self.name = name
+        self.products = []
+
+    def add_products(self, product):
+        self.products.append(product)
+
+
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+soccer_ball = Product("Soccer Ball", 14.99)
+
+sporting_goods = Departments("Sporting Goods")
+
+# sporting_goods.products.append(soccer_ball)
+# if you don't have a method "add_product" use ^^^ but its ugly
+
+sporting_goods.add_products(soccer_ball)
+# if you have a method "add_product" use ^^
+
