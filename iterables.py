8212597bcac2d24ec007e099e40b582cfca5ee8e
jonalynn masters <jonalynna.masters@gmail.com>
2020-04-08 09:27:27 -0700

    Initial Commit

---
+++ iterables.py
@@ -0,0 +1,17 @@
+for i in range(10):
+    print(i)
+
+a = [1, 2, 3, 4]
+
+for i in a:  # Iterable
+    print(i)
+
+a = (1, 2, 3, 4)
+
+for i in a:  # Iterable
+    print(i)
+
+f = open("foo.txt")
+
+for i in f:  # Iterable
+    print(i)
